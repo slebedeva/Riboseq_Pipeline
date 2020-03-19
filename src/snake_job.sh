512e6ad1c75e9f80446d613e1ef9e753bd83af89
Svetlana Lebedeva <svetlana.lebedeva@mdc-berlin.de>
2020-03-19 11:00:44 +0100

    modify config, qsub script and csv, first running version

--- src/snake_job.sh
+++ src/snake_job.sh
@@ -1,15 +1,18 @@
 #! /bin/bash
 #$ -cwd #start from current directory
-#$ -l h_vmem=20G -l h_rt=48:00:00 -l os=centos7 -pe smp 2
+#$ -l h_rt=48:00:00 -l data
 #$ -V #export all the environmental variables into the context of the job
 #$ -j yes #merge the stderr with the stdout
 #$ -o logs/ #stdout, job log
-#$ -m ea # send email beginning, end, and suspension
-#$ -M eelco.meerdink@mdc-berlin.de
-#$ -N 'myjob'
+#$ -m eas # send email beginning, end, and suspension
+#$ -M svetlana.lebedeva@mdc-berlin.de
+#$ -N 'RiboPipe'
 
 
-source ~/.bashrc
-conda activate ribopipebase
+#source ~/.bashrc
+eval "$(/home/lebedeva/miniconda3/bin/conda shell.bash hook)"
+conda activate ribopipe #ribopipebase
+mkdir -p logs
 mkdir -p sge_log
-snakemake -j 10 -k -p --restart-times 1 --max-jobs-per-second 5 -s Snakefile --cluster-config ../src/config_pipeline.json  --rerun-incomplete --use-conda --cluster="qsub -cwd -V -l m_mem_free={cluster.m_mem_free} -l h_rt={cluster.h_rt} -pe {cluster.pe} -j yes -o sge_log" "all"
+snakemake -j 10 -k -p --restart-times 1 --max-jobs-per-second 5 -s Snakefile --cluster-config ../src/config_pipeline.json  --rerun-incomplete --use-conda --cluster="qsub -cwd -V -l m_mem_free={cluster.m_mem_free} -l h_rt={cluster.h_rt} -pe {cluster.pe} -j yes -o sge_log -l data" "all"
+exit 0
